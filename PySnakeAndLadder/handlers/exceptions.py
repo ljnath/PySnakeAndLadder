696b4dd799e754d89e7855b58a304c3e0367c42a
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2021-05-17 00:05:12 +0530

    Renamed exceptions methods

--- PySnakeAndLadder/handlers/exceptions.py
+++ PySnakeAndLadder/handlers/exceptions.py
@@ -1,17 +1,17 @@
 from logger import Logger
 
-class BaseError(Exception):
+class game_assets(Exception):
     def __init__(self):
         Exception.__init__(self)
         self.logger = Logger().get(name = 'snake&ladder')
         
         
-class MissingPlayerNameError(BaseError):
+class MissingPlayerNameException(game_assets):
     def __init__(self):
-        BaseError.__init__(self)
+        game_assets.__init__(self)
         self.logger.exception('Player name is a mandatory property and it cannot be left empty.')
         
-class UnsupportedDiceType(BaseError):
+class UnsupportedDiceTypeException(game_assets):
     def __init__(self, type):
-        BaseError.__init__(self)
+        game_assets.__init__(self)
         self.logger.exception(f'{type} is an un-supported dice type')
